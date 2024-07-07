f7d0460744f8ecf5bd67200190779f550f932417
Francisco Latorre C <flatorrc@ccu.cl>
2024-07-07 13:25:17 -0400

    commit inicial

---
+++ frontend/src/template/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="mt-auto py-5 bg-dark">
+      <div className="container d-flex justify-content-center">
+        <span className="text-muted">Copyright &copy; Trazos de Color 2024</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
